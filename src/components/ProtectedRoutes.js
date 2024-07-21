67e193003bdce674b38a1c24ce365ff6f73d0812
RAJARATNAM PAVITHARAN <para005@ess.ais.ac.nz>
2024-07-21 21:27:34 +1200

    Register view created successfully.

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -4,6 +4,7 @@ import api from "../api";
 import {REFRESH_TOKEN, ACCESS_TOKEN} from "../constants";
 import {useState, useEffect} from "react";
 
+
 function ProtectedRoute({children}) {
     const [isAuthorized, setIsAuthorized] = useState(null)
 
